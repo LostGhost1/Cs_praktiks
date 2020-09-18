f09c3ca9a13ad031e9537e8c7257fb2dce0650a2
LostGhost <askbefore@mail.ru>
2020-09-18 09:57:47 +0300

    Practice 3-1

---
+++ PR03/01/PR3-1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace PR3_1
+{
+  class Program
+  {
+    static bool IsPhone(string s)
+    {
+      return Regex.IsMatch(s, "[+]??\\d[(]??\\d{3}[)]??\\d{3}[-]??\\d{2}[-]??\\d{2}");
+    }
+
+    static bool IsZip(string s)
+    {
+      return Regex.IsMatch(s, "[\\w._-]+@[\\w._-]+");
+    }
+      static void Main(string[] args)
+    {
+      string s = Console.ReadLine();
+      Console.Write(IsPhone(s) ?"IsPhone ":"IsntPhone ");
+      Console.Write(IsZip(s) ? "IsZip " : "IsntZip ");
+    }
+  }
+}
