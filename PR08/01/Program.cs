a29fa2ca3e7f82161344526a4aaa0868920d8595
LostGhost1 <LostGhost1@github.com>
2020-10-30 10:28:41 +0300

    PR08-1

---
+++ PR08/01/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+namespace _01
+{
+  class Program
+  {
+    static void Counting()
+    {
+      for(int i = 0; i < 10; i++)
+      {
+        Console.WriteLine(i + " " + Thread.CurrentThread.ManagedThreadId);
+        Thread.Sleep(10);
+      }
+    }
+    static void Main(string[] args)
+    {
+      ThreadStart starter = new ThreadStart(Counting);
+      Thread first = new Thread(starter);
+      Thread second = new Thread(starter);
+      first.Start();
+      second.Start();
+      first.Join(); 
+      second.Join();
+      Console.Read();
+    }
+  }
+}
