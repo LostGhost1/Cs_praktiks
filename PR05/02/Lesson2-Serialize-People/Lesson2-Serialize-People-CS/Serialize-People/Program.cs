05a7ba140e1be40c82712ae615707980e9dfd6ca
LostGhost1 <LostGhost1@github.com>
2020-10-30 09:56:53 +0300

    PR05-2

---
+++ PR05/02/Lesson2-Serialize-People/Lesson2-Serialize-People-CS/Serialize-People/Program.cs
@@ -0,0 +1,101 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Xml.Serialization;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Serialize_People
+{
+  // A simple program that accepts a name, year, month date,
+  // creates a Person object from that information, 
+  // and then displays that person's age on the console.
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      if (args.Length == 0)
+      {
+        // If they provide no arguments, display the last person
+        Person p = Deserialize();
+        if (p == null) return;
+        Console.WriteLine(p.ToString());
+      }
+      else
+      {
+        try
+        {
+          if (args.Length != 4)
+          {
+            throw new ArgumentException("You must provide four arguments.");
+          }
+
+          DateTime dob = new DateTime(Int32.Parse(args[1]), Int32.Parse(args[2]), Int32.Parse(args[3]));
+          Person p = new Person(args[0], dob);
+          Console.WriteLine(p.ToString());
+
+          Serialize(p);
+        }
+        catch (Exception ex)
+        {
+          DisplayUsageInformation(ex.Message);
+        }
+      }
+    }
+
+    private static void DisplayUsageInformation(string message)
+    {
+      Console.WriteLine("\nERROR: Invalid parameters. " + message);
+      Console.WriteLine("\nSerialize_People \"Name\" Year Month Date");
+      Console.WriteLine("\nFor example:\nSerialize_People \"Tony\" 1922 11 22");
+      Console.WriteLine("\nOr, run the command with no arguments to display that previous person.");
+    }
+
+    private static void Serialize(Person sp)
+    {
+      // Создаемфайлдлясохраненияданных
+      try
+      {
+        FileStream fs = new FileStream("Person.XML", FileMode.Create);
+      
+        // СоздаемобъектXmlSerializerдлявыполнениясериализации
+        XmlSerializer xs = new XmlSerializer(typeof(Person));
+
+        // Используем объект XmlSerializerдля сериализации данных в файл
+        xs.Serialize(fs, sp);
+
+        // Закрываем файл
+        fs.Close();
+      }
+      catch (FileNotFoundException)
+      {
+        return;
+      }
+    }
+
+
+    private static Person Deserialize()
+    {
+      Person dsp = new Person();
+      try
+      {
+        // Создаемфайлдлясохраненияданных
+        FileStream fs = new FileStream("Person.XML", FileMode.Open);
+
+      // СоздаемобъектXmlSerializerдлявыполнениядесериализации
+      XmlSerializer xs = new XmlSerializer(typeof(Person));
+      // Используем объект XmlSerializerдля десериализации данных в файл
+      dsp = (Person)xs.Deserialize(fs);
+
+      // Закрываемфайл
+      fs.Close();
+      return dsp;
+      }
+      catch (FileNotFoundException)
+      {
+        return null;
+      }
+    }
+  }
+}
